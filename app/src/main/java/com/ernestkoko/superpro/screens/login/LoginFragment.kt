8af7f682704b8fbd5f9a41173f23b3b3b514bae1
ernestkoko <ernesteferetin@yahoo.com>
2020-06-25 11:29:33 +0100

    Added nested graph and global action

--- app/src/main/java/com/ernestkoko/superpro/screens/login/LoginFragment.kt
+++ app/src/main/java/com/ernestkoko/superpro/screens/login/LoginFragment.kt
@@ -1,22 +1,23 @@
-package com.ernestkoko.superpro
+package com.ernestkoko.superpro.screens.login
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.ernestkoko.superpro.R
 
 /**
  * A simple [Fragment] subclass.
  */
-class HomeFragment : Fragment() {
+class LoginFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return inflater.inflate(R.layout.fragment_login, container, false)
     }
 
 }
---
+++ app/src/main/java/com/ernestkoko/superpro/screens/login/longin_registration/RegistrationFragment.kt
@@ -0,0 +1,23 @@
+package com.ernestkoko.superpro.screens.login.longin_registration
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.ernestkoko.superpro.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class RegistrationFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_registration, container, false)
+    }
+
+}
